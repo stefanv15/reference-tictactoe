3cf5cba92d9bd7138b8da91fad6d5abd413cb48f
stefanv15 <stefanv15@ru.is>
2016-12-07 14:31:15 +0000

    Commenting code(day5)

--- mybuildscript.sh
+++ mybuildscript.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Cleaning build directory
 echo Cleaning...
 rm -rf ./build
 
 echo Building folders
 
+# Creating Git tag called GIT_COMMIT i.e git tag for my latest Git commit
 if [ -z "$GIT_COMMIT" ]; then
   export GIT_COMMIT=$(git rev-parse HEAD)
   export GIT_URL=$(git config --get remote.origin.url)
@@ -13,25 +15,29 @@ fi
 # Remove .git from url in order to get https link to repo (assumes https url for GitHub)
 export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
 
-
+# Running npm build
 echo Building app
 npm run build
 
 mkdir /build/public
+
 cat > ./build/.env <<_EOF_
 GIT_COMMIT=$GIT_COMMIT
 _EOF_
 
+# Error message if npm build fails
 rc=$?
 if [[ $rc != 0 ]] ; then
     echo "Npm build failed with exit code " $rc
     exit $rc
 fi
 
+
 cat > ./build/githash.txt <<_EOF_
 $GIT_COMMIT
 _EOF_
 
+# Creating .html file with following information
 cat > ./build/public/version.html << _EOF_
 <!doctype html>
 <head>
@@ -45,23 +51,29 @@ cat > ./build/public/version.html << _EOF_
 </body>
 _EOF_
 
+# Copying files into build directory
 cp ./Dockerfile ./build/
 cp ./package.json ./build/
 cp ./docker-compose.yaml ./build/
 cp ./migrate.sh ./build/
 
+# Following commands are made inside build directory
 cd build
-echo Building docker image
 
+echo Building docker image
 sudo docker build -t stefanv15/tictactoe:$GIT_COMMIT .
 
+# Error message for docker build failure
 rc=$?
 if [[ $rc != 0 ]] ; then
     echo "Docker build failed " $rc
     exit $rc
 fi
 
+# Pushing docker image
 sudo docker push stefanv15/tictactoe:$GIT_COMMIT
+
+# Error message for docker push failure
 rc=$?
 if [[ $rc != 0 ]] ; then
     echo "Docker push failed " $rc
