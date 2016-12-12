9988d82ffb6a2d27beffffba2ade0efe138d079b
stefanv15 <stefanv15@ru.is>
2016-12-12 15:34:24 +0000

    Fix buildscript for jenkins

--- mybuildscript.sh
+++ mybuildscript.sh
@@ -15,8 +15,13 @@ fi
 # Remove .git from url in order to get https link to repo (assumes https url for GitHub)
 export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
 
+
+
 # Running npm build
 echo Building app
+cd ./client
+npm install
+cd ..
 npm run build
 
 mkdir /build/public
