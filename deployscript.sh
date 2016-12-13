d27f8e0b3060034bdf0f4679d173ffd4489036cb
stefanv15 <stefanv15@ru.is>
2016-12-13 23:33:40 +0000

    fix path to pem file in deployscript

--- deployscript.sh
+++ deployscript.sh
@@ -5,13 +5,13 @@ mv ../Commit/.env .
 ls -a
 
 # Copy files to AWS
-scp -o StrictHostKeyChecking=no -i "/home/ubuntu/pem.pem" ./docker-compose.yaml ec2-user@ec2-54-229-178-217-eu-west-1.compute.amazonaws.com:~/docker-compose.yaml
+scp -o StrictHostKeyChecking=no -i "/home/ubuntu/pem.pem" ./docker-compose.yaml ec2-user@ec2-54-229-178-217.eu-west-1.compute.amazonaws.com:~/docker-compose.yaml
 
-scp -o StrictHostKeyChecking=no -i "/home/ubuntu/pem.pem" ./.env ec2-user@ec2-54-229-178-217-eu-west-1.compute.amazonaws.com:~/.env
+scp -o StrictHostKeyChecking=no -i "/home/ubuntu/pem.pem" ./.env ec2-user@ec2-54-229-178-217.eu-west-1.compute.amazonaws.com:~/.env
 
 # Connecting AWS server with public key from pem.pem file
 echo Connecting to AWS server
-ssh -o StrictHostKeyChecking=no -i "/home/ubuntu/pem.pem" ec2-user@ec2-54-229-178-217-eu-west-1.compute.amazonaws.com "docker-compose up -d"
+ssh -o StrictHostKeyChecking=no -i "/home/ubuntu/pem.pem" ec2-user@ec2-54-229-178-217.eu-west-1.compute.amazonaws.com "docker-compose up -d"
 
 echo "Done"
 
