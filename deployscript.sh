bb78a5314b7a1e0ed58708a952b624ccd10ffb79
stefanv15 <stefanv15@ru.is>
2016-12-13 13:22:58 +0000

    Added Deployment script

---
+++ deployscript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mv ../Commit/docker-compose.yaml .
+mv ../Commit/.env .
+ls
+
+# Copy files to AWS
+scp -o StrictHostKeyChecking=no -i "pem.pem" ./docker-compose.yaml ec2-user@ec2-54-229-178-217-eu-west-1.compute.amazonaws.com:~/docker-compose.yaml
+
+scp -o StrictHostKeyChecking=no -i "pem.pem" ./.env ec2-user@ec2-54-229-178-217-eu-west-1.compute.amazonaws.com:~/.env
+
+# Connecting AWS server with public key from pem.pem file
+echo Connecting to AWS server
+ssh -i "pem.pem" ec2-user@ec2-54-229-178-217.eu-west-1.compute.amazonaws.com
+ls
+
+# Start docker containers
+docker-compose up -d
+exit
+
+
+echo "Done"
