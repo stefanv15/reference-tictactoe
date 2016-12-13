37a454496c0a826d20c7f3670377ad9e697bce08
stefanv15 <stefanv15@ru.is>
2016-12-13 15:36:00 +0000

    More fix in deployscript.sh

--- deployscript.sh
+++ deployscript.sh
@@ -11,7 +11,7 @@ scp -o StrictHostKeyChecking=no -i "pem.pem" ./.env ec2-user@ec2-54-229-178-217-
 
 # Connecting AWS server with public key from pem.pem file
 echo Connecting to AWS server
-ssh -i "pem.pem" ec2-user@ec2-54-229-178-217-eu-west-1.compute.amazonaws.com "docker-compose up -d"
+ssh -o StrictHostKeyChecking=no -i "pem.pem" ec2-user@ec2-54-229-178-217-eu-west-1.compute.amazonaws.com "docker-compose up -d"
 
 echo "Done"
 
