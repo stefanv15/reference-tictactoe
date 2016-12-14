d64de50926f7c64cef148bbdfe8f56822572a0f1
stefanv15 <stefanv15@ru.is>
2016-12-14 13:11:12 +0000

    Deployscript fix

--- deployscript.sh
+++ deployscript.sh
@@ -11,7 +11,7 @@ echo "------------------------------------"
 # Copy files to AWS
 scp -o StrictHostKeyChecking=no -i "/home/ubuntu/pem.pem" ./docker-compose.yaml ec2-user@ec2-54-229-178-217.eu-west-1.compute.amazonaws.com:~/docker-compose.yaml
 
-scp -o StrictHostKeyChecking=no -i "/home/ubuntu/pem.pem" ./.env ec2-user@ec2-54-229-178-217.eu-west-1.compute.amazonaws.com:~/build/.env
+scp -o StrictHostKeyChecking=no -i "/home/ubuntu/pem.pem" ./.env ec2-user@ec2-54-229-178-217.eu-west-1.compute.amazonaws.com:~/.env
 
 # Connecting AWS server with public key from pem.pem file
 echo Connecting to AWS server
